c0f07f84a23db016486165c93e8b99bc6d39d9fb
nmikrou <nmikrou@ots.gr>
2023-08-13 23:44:27 +0300

    New Portfolio

---
+++ countdown.js
@@ -0,0 +1,18 @@
+const completionDate = new Date(2023, 8, 10, 12, 0, 0); // Year, Month (0-11), Day, Hour, Minute, Second
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const timeRemaining = completionDate - now;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days < 10 ? '0' + days : days;
+    document.getElementById('hours').innerText = hours < 10 ? '0' + hours : hours;
+    document.getElementById('minutes').innerText = minutes < 10 ? '0' + minutes : minutes;
+    document.getElementById('seconds').innerText = seconds < 10 ? '0' + seconds : seconds;
+}
+
+setInterval(updateCountdown, 1000);
